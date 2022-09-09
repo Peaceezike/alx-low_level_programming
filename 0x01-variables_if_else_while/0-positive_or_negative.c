50fbb657bee3365a82ea5cada933f3d988f7106f
Peaceezike <peaceezike50@gmail.com>
2022-09-09 10:22:04 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+        srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
@@ -26,3 +28,6 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
+
+	return (0);
+}
