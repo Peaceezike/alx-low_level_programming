ce0bfff49fe6474e159f8bb925a85f15035865d4
Peaceezike <peaceezike50@gmail.com>
2022-09-13 11:42:59 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,46 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - print if the number is postive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
+ * jack_bauer - prints every minute of the day
+ * Description: prints every minute of the day
+ * Return: void
  */
-int main(void)
+void jack_bauer(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-	putchar(c);
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-	putchar(c);
-}
-putchar('\n');
-return (0);
+	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
+
+	hours_max = 58;
+	hours_tens = '0';
+	while (hours_tens < '3')
+	{
+		if (hours_tens == '2')
+		{
+			hours_max = '4';
+		}
+		hours_ones = '0';
+		while (hours_ones < hours_max)
+		{
+			minutes_tens = '0';
+			while (minutes_tens < '6')
+			{
+				minutes_ones = '0';
+				while (minutes_ones < 58)
+				{
+                                     _putchar(hours_tens);
+					_putchar(hours_ones);
+					_putchar(':');
+					_putchar(minutes_tens);
+					_putchar(minutes_ones);
+					_putchar('\n');
+					minutes_ones++;
+				}
+				minutes_ones = '0';
+				minutes_tens++;
+			}
+			minutes_tens = '0';
+			hours_ones++;
+		}
+		hours_ones = '0';
+		hours_tens++;
+	}
 }
