4a18df6263a2dd9566fd091748b5b2a41e192d92
Peaceezike <peaceezike50@gmail.com>
2022-09-13 09:15:02 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - print putchar
- * Return: always 0
+ * main - Program that prints _putchar, followed by a new line
+ * Return: Always 0
  */
-
 int main(void)
 {
 	putchar('');
