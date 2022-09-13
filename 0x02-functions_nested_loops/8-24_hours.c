746382ec80c3b87b99676b0cd2b7301c8a8d41a4
Peaceezike <peaceezike50@gmail.com>
2022-09-13 10:01:57 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,35 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- *
- * Return: Always 0.
- */
+* jack_bauer - prints every minute of the day
+*
+* Return: no return
+*/
 
 void jack_bauer(void)
 {
-	int hr;
-	int min;
+	int a, b, c, d;
 
-	for (hr = 0; hr < 24; hr++)
+	for (a = 0; a <= 2; a++)
 	{
-		for (min = 0; min < 60; min++)
+		for (b = 0; b <= 9; b++)
 		{
-			_putchar((hr / 10) + '0');
-			_putchar((hr % 10) + '0');
-			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min % 10) + '0');
-			_putchar('\n');
-		}
-	}
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a >= 2 && b >= 4)
+
+					break;
+
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+                                }
+                       }
+               }
+        }
 }
