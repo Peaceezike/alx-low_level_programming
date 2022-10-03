3753912c13a65453767e3613e13ddfedae5a1d45
Peaceezike <peaceezike50@gmail.com>
2022-10-03 00:48:27 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,46 +1,38 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: The string to be concatenated upon.
- * @s2: The string to be concatenated to s1.
- *
- * Return: If concatenation fails - NULL.
- *         Otherwise - a pointer the newly-allocated space in memory
- *                     containing the concatenated strings.
- */
+* str_concat - a function that concatenates two strings.
+*@s1:First string
+*@s2:Second string
+*
+*Return: NULL in case of failure , but pointer to new string in
+*case of success
+*/
+
 char *str_concat(char *s1, char *s2)
 {
-	char *concat;
-	unsigned int i;
-	unsigned int j;
-	unsigned int length_S1;
-	unsigned int length_S2;
-	unsigned int length_c;
+	char *concat_str;
+	int index, concat_index = 0,  len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
-	for (length_S1 = 0; s1[length_S1] != '\0'; length_S1++)
-		;
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
 
-	for (length_S2 = 0; s2[length_S2] != '\0'; length_S2++)
-		;
+	concat_str = malloc(sizeof(char) * len);
 
-	length_c = length_S1 + length_S2;
-
-	concat = malloc((length_c + 1) * sizeof(char));
-	if (concat == NULL)
+	if (concat_str == NULL)
 		return (NULL);
 
-	for (i = 0; i < length_S1; i++)
-		concat[i] = s1[i];
+	for (index = 0; s1[index]; index++)
+		concat_str[concat_index++] = s1[index];
 
-	for (j = 0; j < length_S2; i++, j++)
-		concat[i] = s2[j];
-        
-        return (concat);
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s2[index];
+
+	return (concat_str);
 }
