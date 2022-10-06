5f6ae3a77facdaace88612ac78e6201959e7ff50
Peaceezike <peaceezike50@gmail.com>
2022-10-06 11:46:31 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 success
-*/
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
 unsigned long mul;
